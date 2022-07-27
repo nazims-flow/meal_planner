128bd5b6a720af3853d535db4bca50356a285556
nazims-flow <nazimmazumder68@gmail.com>
2022-07-27 16:37:24 +0530

    updated searchfoodItem

--- controllers/foodItemController.js
+++ controllers/foodItemController.js
@@ -25,3 +25,32 @@ exports.addfoodItem=BigPromise(async (req , res, next) =>{
  
 
 })
+
+exports.searchfoodItem=BigPromise(async (req , res, next) =>{
+    
+    const {calories} =req.body
+    const highCalorie=calories+100;
+    const lowCalorie=calories-100;
+    const FoodItem=await Fooditem.find();
+    var food=[]
+    function checkProtein(protein,calories){
+        const protien_conversion=4*protein;
+        if(pr<=(calo*0.3) && pr>=(calory*0.2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    for (var i=0; i < FoodItem.length; i++) {
+        if(FoodItem[i].calories<highCalorie&&FoodItem[i].calories>lowCalorie){
+            if(checkProtein(FoodItem[i].protein,FoodItem[i].calories)){
+            food.push(FoodItem[i]);
+            }
+        }
+     }
+
+   res.send(ans);
+ 
+
+})
--- routes/foodItem.js
+++ routes/foodItem.js
@@ -1,10 +1,10 @@
 const express= require('express');
 const router = express.Router();
 
-const{addfoodItem}= require('../controllers/foodItemController')
+const{addfoodItem, searchfoodItem}= require('../controllers/foodItemController')
 
 router.route("/addfooditem").post(addfoodItem);
-
+router.route("/searchfoodItem").get(searchfoodItem)
 
 
 module.exports=router
